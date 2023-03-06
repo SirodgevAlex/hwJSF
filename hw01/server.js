2faf389482f6d07f809143d1169ae214bce6871c
aleksandrsirodzhev <sirodgevolimpwinner@yandex.ru>
2023-03-06 09:39:43 +0300

    дополнил, что сделал

--- hw01/server.js
+++ hw01/server.js
@@ -6,9 +6,9 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end('Hello World and JB');
 });
 
 server.listen(port, hostname, () => {
-  console.log('Server running at http://${hostname}:${port}/');
+  console.log('Server running at http://localhost:3000/');
 });
